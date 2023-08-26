cf5ea5f2ac26da1e9a6c25be7793643a10da75f6
petekostrytsya <petekostrytsya@gmail.com>
2023-08-27 00:24:08 +0300

    add email register and send

--- models/user.js
+++ models/user.js
@@ -33,7 +33,15 @@ const userSchema = new Schema({
     avatarURL: {
         type: String,
         required: true,
-    }
+    },
+    verify: {
+        type: Boolean,
+        default: false,
+    },
+    verificationToken: {
+    type: String,
+    required: [true, 'Verify token is required'],
+    },
 }, { versionKey: false, timestamps: true });
 
 userSchema.post('save', handleMongooseError);
@@ -44,6 +52,10 @@ const registerSchema = Joi.object({
     password: Joi.string().min(8).required(),
 })
 
+const emailSchema = Joi.object({
+   email: Joi.string().pattern(emailRegexp).required() 
+})
+
 const loginSchema = Joi.object({
     email: Joi.string().pattern(emailRegexp).required(),
     password: Joi.string().min(8).required(),
@@ -51,6 +63,7 @@ const loginSchema = Joi.object({
 
 const schemas = {
     registerSchema,
+    emailSchema,
     loginSchema,
 }
 
